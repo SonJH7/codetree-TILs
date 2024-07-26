3fe7fc346752c5da6f47459c77c46ec31e7109a2
SonJH7 <129269335+SonJH7@users.noreply.github.com>
2024-07-26 21:26:10 +0900

    Title: 별 찍는 것을 5번 반복하기, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240726/별 찍는 것을 5번 반복하기/repeat-shooting-the-stars-five-times.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+void star(){
+    cout << "**********";
+}
+int main() {
+    star();
+    star();
+    star();
+    star();
+    star();
+    return 0;
+}
