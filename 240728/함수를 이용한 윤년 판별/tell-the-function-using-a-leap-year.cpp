43b0646ddccae894561360a9f0bf710e17d64e85
SonJH7 <129269335+SonJH7@users.noreply.github.com>
2024-07-28 20:04:33 +0900

    Title: 함수를 이용한 윤년 판별, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240728/함수를 이용한 윤년 판별/tell-the-function-using-a-leap-year.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+const char* yunnean(int a){
+    if(a % 4 != 0 && (a % 100 == 0 && a % 400 != 0) )
+        return "false";
+    else
+        return "true";
+}
+
+int main() {
+    int a;
+    cin >> a;
+    cout << yunnean(a);
+    return 0;
+}
