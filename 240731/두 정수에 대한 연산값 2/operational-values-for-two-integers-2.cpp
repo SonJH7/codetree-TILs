198c2848b0cff7e1074696ac3cfcee0e9843418a
SonJH7 <129269335+SonJH7@users.noreply.github.com>
2024-07-31 23:07:51 +0900

    Title: 두 정수에 대한 연산값 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240731/두 정수에 대한 연산값 2/operational-values-for-two-integers-2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+void mycheck(int a, int b){
+    if(a>b)
+        cout << a*2 << b + 10;
+    else
+        cout << a+10 << b*2;
+    
+}
+
+int main() {
+    int a,b;
+    cin >> a >> b;
+    mycheck(a,b);
+    return 0;
+}
