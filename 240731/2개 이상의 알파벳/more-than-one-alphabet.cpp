ab11e8b8953518b71624d231e6f96e9a47641ce8
SonJH7 <129269335+SonJH7@users.noreply.github.com>
2024-07-31 23:03:20 +0900

    Title: 2개 이상의 알파벳, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240731/2개 이상의 알파벳/more-than-one-alphabet.cpp
+++ 240731/2개 이상의 알파벳/more-than-one-alphabet.cpp
@@ -6,6 +6,7 @@ using namespace std;
 int my_check(int arr[]){
     int check = 0;
     for(int i=0; i<26; i++){
+        
         if(arr[i]>0)
             check++;
     }
@@ -19,7 +20,8 @@ int main() {
     for(int i=0; i<s.length(); i++){
         arr[s[i]-'a']++;
     }
-    if(my_check(arr)>1)
+    int check = my_check(arr);
+    if(check>1)
         cout << "Yes";
     else
         cout << "No";
