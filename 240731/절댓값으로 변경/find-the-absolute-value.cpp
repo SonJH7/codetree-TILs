b1f052eec1a69a9ad89a886b5e271ca8e484dd49
SonJH7 <129269335+SonJH7@users.noreply.github.com>
2024-07-31 22:50:24 +0900

    Title: 절댓값으로 변경, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240731/절댓값으로 변경/find-the-absolute-value.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+void my_abs(int arr[])
+{   
+    
+    for(int i=0; i<n; i++){
+        if( arr[i] > 0)
+            cout << arr[i] << " ";
+        else
+            cout << arr[i] * (-1) << " ";
+    }
+}
+int main() {
+    int n;
+    cin >> n;
+    int arr[n];
+
+    for(int i=0; i<n; i++){
+        cin >> arr[i];
+        my_abs(arr);
+    }
+
+    return 0;
+}
