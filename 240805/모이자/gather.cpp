320418942bd01e8eb5af327ee23a92230f009b13
SonJH7 <129269335+SonJH7@users.noreply.github.com>
2024-08-05 21:54:03 +0900

    Title: 모이자, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240805/모이자/gather.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int house_count;
+    cin >> house_count;
+    int arr[house_count];
+    
+    for(int i=0; i<house_count; i++)
+        cin >> arr[i];
+    int min_distance = 10000;
+    for(int i=0; i<house_count; i++){
+        int str_moim = i;
+        int test_distance = 0;
+        for(int j=0; j<house_count; j++){
+            test_distance += arr[j] * abs(j-i);    
+        }
+        min_distance = min(min_distance, test_distance);
+    }
+    cout << min_distance;
+    return 0;
+}
