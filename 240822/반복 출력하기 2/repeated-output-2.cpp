6704826b570dc6665b3dd4d15811d2506db8b3d6
SonJH7 <129269335+SonJH7@users.noreply.github.com>
2024-08-22 14:16:08 +0900

    Title: 반복 출력하기 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240822/반복 출력하기 2/repeated-output-2.cpp
+++ 240822/반복 출력하기 2/repeated-output-2.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 void print_HelloWorld(int N, int n){
     if(n < N){
-        cout << "Helloworld\n";
+        cout << "HelloWorld\n";
         n++;
         print_HelloWorld(N,n);
     }
