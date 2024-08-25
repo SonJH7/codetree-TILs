10cf74b9d292ef0155e93a7d73083f6eacde661b
SonJH7 <129269335+SonJH7@users.noreply.github.com>
2024-08-25 21:12:03 +0900

    Title: 재귀함수를 이용한 별 출력, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240825/재귀함수를 이용한 별 출력/star-output-with-recursive-function.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+void print_star(int n, int i){
+    if(i < n){
+        for(int j=0; j<i; j++)
+            cout << "*" ;
+        cout << endl;
+    i++;
+    print_star(n, i);
+    }
+    
+}
+int main() {
+    int n, i=1;
+    cin >> n;
+    print_star(n,i);
+    return 0;
+}
